9909d1f0aa70e28d7f9218a2656157d41a7517b2
ngh7mr3 <20752642+ngh7mr3@users.noreply.github.com>
2022-03-20 17:43:32 +0300

    set up json response

--- poco-rest-app/http_server/RequestHandler.cpp
+++ poco-rest-app/http_server/RequestHandler.cpp
@@ -1,21 +1,23 @@
 #include "RequestHandler.h"
 
 #include <iostream>
+#include <Poco/JSON/Object.h>
 
 using namespace Poco::Net;
 using http_server::RequestHandler;
 
 void RequestHandler::handleRequest(HTTPServerRequest &req, HTTPServerResponse &resp)
 {
-  std::cout << "[ ] Got request for URI: " << req.getURI() << std::endl;
+    std::cout << '[' << req.getMethod() << ']' << ' ' << req.getURI() << std::endl;
 
-  resp.setStatus(HTTPResponse::HTTP_OK);
-  resp.setContentType("text/html");
+    resp.setStatus(HTTPResponse::HTTP_OK);
+    resp.setContentType("application/json");
 
+    Poco::JSON::Object::Ptr root = new Poco::JSON::Object();
+    root->set("request_uri", req.getURI());
+    root->set("request_method", req.getMethod());
 
-  std::ostream& out = resp.send();
-
-  out << "<h1>Hello world!</h1>";
-
-  out.flush();
+    std::ostream& out = resp.send();
+    Poco::JSON::Stringifier::stringify(root, out);
+    out.flush();
 }
