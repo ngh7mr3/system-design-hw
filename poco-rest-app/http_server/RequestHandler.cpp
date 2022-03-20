60f46fdbbbc4ecd442808ffb5394a6b5f0dab509
ngh7mr3 <20752642+ngh7mr3@users.noreply.github.com>
2022-03-20 21:01:55 +0300

    overload send method

--- poco-rest-app/http_server/RequestHandler.cpp
+++ poco-rest-app/http_server/RequestHandler.cpp
@@ -1,7 +1,6 @@
 #include "RequestHandler.h"
 
 #include <iostream>
-#include <Poco/JSON/Object.h>
 
 using namespace Poco::Net;
 using http_server::RequestHandler;
@@ -10,14 +9,18 @@ void RequestHandler::handleRequest(HTTPServerRequest &req, HTTPServerResponse &r
 {
     std::cout << '[' << req.getMethod() << ']' << ' ' << req.getURI() << std::endl;
 
-    resp.setStatus(HTTPResponse::HTTP_OK);
-    resp.setContentType("application/json");
-
     Poco::JSON::Object::Ptr root = new Poco::JSON::Object();
     root->set("request_uri", req.getURI());
     root->set("request_method", req.getMethod());
 
+    send(resp, HTTPResponse::HTTP_OK, root);
+}
+
+void RequestHandler::send(HTTPServerResponse& resp, HTTPResponse::HTTPStatus status, Poco::JSON::Object::Ptr data)
+{
+    resp.setStatus(status);
+    resp.setContentType("application/json");
     std::ostream& out = resp.send();
-    Poco::JSON::Stringifier::stringify(root, out);
+    Poco::JSON::Stringifier::stringify(data, out);
     out.flush();
 }
--- poco-rest-app/http_server/RequestHandler.h
+++ poco-rest-app/http_server/RequestHandler.h
@@ -3,6 +3,7 @@
 #include <Poco/Net/HTTPRequestHandler.h>
 #include <Poco/Net/HTTPServerRequest.h>
 #include <Poco/Net/HTTPServerResponse.h>
+#include <Poco/JSON/Object.h>
 
 namespace http_server
 {
@@ -11,8 +12,13 @@ class RequestHandler : public Poco::Net::HTTPRequestHandler
 {
 public:
   virtual void handleRequest(
-      Poco::Net::HTTPServerRequest &req, 
-      Poco::Net::HTTPServerResponse &resp);
+    Poco::Net::HTTPServerRequest &req, 
+    Poco::Net::HTTPServerResponse &resp);
+
+  virtual void send(
+    Poco::Net::HTTPServerResponse& resp,
+    Poco::Net::HTTPResponse::HTTPStatus status,
+    Poco::JSON::Object::Ptr data);
 };
 
 }
